0b22a0488d120d5df753fb738aeb6773d6d5a59e
Juan Martin <juansm91@gmail.com>
2021-05-16 22:31:19 -0300

    footer padding

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import { FaHeart, FaCode } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <footer className="my-12 text-center">
+    <footer className="mt-12 pb-3 text-center">
       © {new Date().getFullYear()}. Desde Buenos Aires al world con <FaHeart className="text-red-600" /> y <FaCode className="text-blue-800" />
     </footer>
   )
